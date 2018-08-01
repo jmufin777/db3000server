e1537a91d6ed8caff94fd2dc764f8ae5ad8d4b10
jmufin777@gmail.com <jmufin777@gmail.com>
2018-08-01 02:43:42 +0200

    modules transaction

--- controllers/list_Modules.js
+++ controllers/list_Modules.js
@@ -1,30 +1,27 @@
 //const {User} = require('../models')
 const config = require('../config/config')
 const {pool, client } = require('../db')
-
-
 const tabname = 'list_modules'
+
 module.exports = {
-    async all (req, res) {
+    async all (req, res, next) {
     try {
     //  const {login, password} = req.body
     console.log('\n --------------\n')
         console.log('\n req:', req)
         console.log('\n --------------\n')
-        console.log('list_modules all ',`select * from ${tabname}`)
         const client = await pool.connect()
-         await client.query(`select * from ${tabname} where 1=$1`  ,[1 ],(err, response) => {
+         await client.query(`select * from ${tabname} where 1=$1 order by category`  ,[1 ],(err, response) => {
           //console.log(response)
            if (response.rowCount == 0)   {
-              res.status(433).send({error: 'Moduly neobsahuji zaznamy '})
-             //res.json({info: 'ok', data: 'sracka'}); 
+              res.send({info: 0})
+              //res.json({info: 'error', data: '433' }); 
            } else {
-              res.json({info: 'ok', data: response.rows}); 
+              res.json({info: 1, data: response.rows}); 
            }
              if (err) return next(err);
          })
          await client.release() 
-
     } catch (err) {
         console.log(err)
         res.status(400).send({
@@ -32,43 +29,31 @@ module.exports = {
         })
     }
   },
-
-
-  async init (req, res) {
+  async init (req, res, next) {
     let neco = ''
     const user = req.body.user
     try {
     //  const {login, password} = req.body
-        console.log('\n --------------\n')
         console.log('\n req:', req.body.user )
-
-
         console.log('\n --------------\n')
-        console.log('list_modules init ',`select * from ${tabname}`)
         const client = await pool.connect()
+        client.query(`begin work  `  ,[],(err, response ) => {
+        })
+        client.query(`delete from   ${tabname}  `  ,[],(err, response ) => {
+        })
+
         await req.body.data.forEach(element => {
-            
-          console.log(JSON.stringify(element))
           neco = JSON.stringify(element)
-          //neco = JSON.parse(neco)
-          console.log(neco)
-                               
-          client.query(`insert into  ${tabname} (nazev,items,user_insert ) values ($1 , $2 ,$3 ) `  ,[element[0], neco, user ],(err, response, next ) => {
-           
-            if (err) return next(err);
-           
-           console.log(response, err)
-          //     res.json({info: 'ok'}); 
-              // if (err) return next(err);
+          client.query(`insert into  ${tabname} (nazev,items,user_insert ) values ( $1 , $2 ,$3 )  `  ,[element[0], neco, user ],(err, response ) => {
+            if (err) {
+              return next(err)
+            } 
           })
-          
-
         });
+        client.query(`commit  `  ,[],(err, response ) => {
+        })
         await client.release() 
-
-
-
-
+        res.json({info:'OK'})
     } catch (err) {
         console.log(err)
         res.status(400).send({
