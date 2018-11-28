3370e9c238b6d0517341f275ad7d05c91a794f0f
jmufin777@gmail.com <jmufin777@gmail.com>
2018-11-28 02:25:52 +0100

    tabulka excel 44

--- controllers/ico.js
+++ controllers/ico.js
@@ -5,32 +5,78 @@ var lErr= false
 module.exports = {
     async all (req, response) {
       var firma=''
-      const objFirma ={}
-      request(`https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=${req.query.id}`, { json: false }, (err, res, body) => {
+      const objFirma ={
+
+          datumvypisu : '' ,
+          ico         : '' ,
+          dic         : '' ,
+          nazev       : '' ,
+          ulicecela   : '' ,
+          obeccela    : '' ,
+          obec        : '' ,
+          ulice       : '' ,
+          psc         : '' ,
+          obec        : '' ,
+          cp1         : '' ,
+          cp2         : '' ,
+          aktivni     : '' ,
+      }
+      const url= `https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=${req.query.id}`
+      request(`${url}`, { json: false }, (err, res, body) => {
      if (err) { return console.log(err); }
       firma = body
 
+       var datumvypisu = firma.match(/(?=D:ADB).*\s*([^^]*)<\/D\:ADB>/)
+       var ico         = firma.match(/(?=D\:ICO zdroj="([^^]*)").*\s*([^^]*)<\/D\:ICO>/)
+       var dic         = firma.match(/(?=D:DIC zdroj="([^^]*)").*\s*([^^]*)<\/D\:DIC>/)
+       var nazev       = firma.match(/(?=D:OF zdroj="([^^]*)").*\s*([^^]*)<\/D\:OF>/)
+       var ulicecela   = firma.match(/(?=D:UC).*\s*([^^]*)<\/D\:UC>/)
+       var obeccela    = firma.match(/(?=D:PB).*\s*([^^]*)<\/D\:PB>/)
+       var obec        = firma.match(/(?=D:NMC).*\s*([^^]*)<\/D\:NMC>/)
+
+       var ulice       = firma.match(/(?=D:NU).*\s*([^^]*)<\/D\:NU>/)
+       var psc         = firma.match(/(?=D:PSC).*\s*([^^]*)<\/D\:PSC>/)
+       var obec        = firma.match(/(?=D:NMC).*\s*([^^]*)<\/D\:NMC>/)
+       var cp1         = firma.match(/(?=D:CD).*\s*([^^]*)<\/D\:CD>/)
+       var cp2         = firma.match(/(?=D:CO).*\s*([^^]*)<\/D\:CO>/)
+       var aktivni     = firma.match(/(?=D:SSU).*\s*([^^]*)<\/D\:SSU>/)
+
+
+       //Kdyz to nevyjde
+       if (!obec){
+         obec        = firma.match(/(?=D:N>).*\s*([^^]*)<\/D\:N>/)
+
+       //  dic = ['a','b<a']
+       }
+       console.log(dic)
+
+
+      // https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=60216654
 
       try {
-       objFirma.datumvypisu = firma.match(/(?=D:ADB).*\s*([^^]*)<\/D\:ADB>/)[0].split(/[<>]/)[1]
-       objFirma.ico         = firma.match(/(?=D\:ICO zdroj="OR").*\s*([^^]*)<\/D\:ICO>/)[0].split(/[<>]/)[1]
-       objFirma.dic         = firma.match(/(?=D:DIC zdroj="DPH").*\s*([^^]*)<\/D\:DIC>/)[0].split(/[<>]/)[1]
-       objFirma.nazev       = firma.match(/(?=D:OF zdroj="OR").*\s*([^^]*)<\/D\:OF>/)[0].split(/[<>]/)[1]
-       objFirma.ulicecela   = firma.match(/(?=D:UC).*\s*([^^]*)<\/D\:UC>/)[0].split(/[<>]/)[1]
-       objFirma.obeccela    = firma.match(/(?=D:PB).*\s*([^^]*)<\/D\:PB>/)[0].split(/[<>]/)[1]
-       objFirma.obec        = firma.match(/(?=D:NMC).*\s*([^^]*)<\/D\:NMC>/)[0].split(/[<>]/)[1]
-       objFirma.ulice       = firma.match(/(?=D:NU).*\s*([^^]*)<\/D\:NU>/)[0].split(/[<>]/)[1]
-       objFirma.psc         = firma.match(/(?=D:PSC).*\s*([^^]*)<\/D\:PSC>/)[0].split(/[<>]/)[1]
-       objFirma.obec        = firma.match(/(?=D:NMC).*\s*([^^]*)<\/D\:NMC>/)[0].split(/[<>]/)[1]
-       objFirma.cp1         = firma.match(/(?=D:CD).*\s*([^^]*)<\/D\:CD>/)[0].split(/[<>]/)[1]
-       objFirma.cp2         = firma.match(/(?=D:CO).*\s*([^^]*)<\/D\:CO>/)[0].split(/[<>]/)[1]
-       objFirma.aktivni     = firma.match(/(?=D:SSU).*\s*([^^]*)<\/D\:SSU>/)[0].split(/[<>]/)[1]
+       objFirma.datumvypisu = (datumvypisu) ? datumvypisu    [0].split(/[<>]/)[1]:''
+       objFirma.ico         = (ico        ) ? ico            [0].split(/[<>]/)[1]:''
+       objFirma.dic         = (dic        ) ? dic            [0].split(/[<>]/)[1]:''
+       objFirma.nazev       = (nazev      ) ? nazev          [0].split(/[<>]/)[1]:''
+       objFirma.ulicecela   = (ulicecela  ) ? ulicecela      [0].split(/[<>]/)[1]:''
+       objFirma.obeccela    = (obeccela   ) ? obeccela       [0].split(/[<>]/)[1]:''
+       objFirma.obec        = (obec       ) ? obec           [0].split(/[<>]/)[1]:''
+       objFirma.ulice       = (ulice      ) ? ulice          [0].split(/[<>]/)[1]:''
+       objFirma.psc         = (psc        ) ? psc            [0].split(/[<>]/)[1]:''
+       objFirma.obec        = (obec       ) ? obec           [0].split(/[<>]/)[1]:''
+       objFirma.cp1         = (cp1        ) ? cp1            [0].split(/[<>]/)[1]:''
+       objFirma.cp2         = (cp2        ) ? cp2            [0].split(/[<>]/)[1]:''
+       objFirma.aktivni     = (aktivni    ) ? aktivni        [0].split(/[<>]/)[1]:'Neuvedeno'
+       if (objFirma.ulicecela.match(/^[0-9]/) && ! objFirma.ulicecela.match(/[a-z]/i) ){
+        objFirma.ulicecela=objFirma.obec  + ' ' + objFirma.ulicecela 
+       }
        
        console.log(objFirma)
        
         response.json(objFirma)
       } catch(e)  {
-         response.json({err: `Nejsou data pro subjekt ${req.query.id}`}) 
+         console.log(e)
+         response.json({err: `Nejsou k dispozici data pro subjekt ${req.query.id}`}) 
       }
        // console.log(body.explanation);
       });
